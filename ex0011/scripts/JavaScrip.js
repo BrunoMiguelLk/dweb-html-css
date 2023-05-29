205ded4ab6a73e9b40426cd10abecd1b78e88e1d
BrunoMiguelLk <133688600+BrunoMiguelLk@users.noreply.github.com>
2023-05-29 16:55:31 -0300

    tarefa
    Apredendp JavaScript

---
+++ ex0011/scripts/JavaScrip.js
@@ -0,0 +1,11 @@
+function Ação1() {
+    area.innerHTML = 'Voce clicou no Ação 1!'
+}
+
+function Ação2() {
+    area.innerHTML = `Voce clicou no Ação 2!`
+}
+
+function Ação3() {
+    area.innerHTML = 'Voce clicou no Ação 3!'
+}
