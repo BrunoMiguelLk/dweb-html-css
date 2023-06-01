c6d63a34bc2d05584d124c6508088a2d10f637ac
BrunoMiguelLk <133688600+BrunoMiguelLk@users.noreply.github.com>
2023-06-01 16:56:34 -0300

    JavaScript
    Boa tarde neymar

---
+++ ex0018/scripts/verificaIdade.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+    var anoUsuario = Number(document.getElementById('input_ano').value)
+    var resultado = document.getElementById('resultado')
+    var idade = anoAtual - anoUsuario
+
+    resultado.innerHTML = `<p>Voce esta com ${idade} anos.</p>`
+
+    var input_radio = document.getElementsByName('radsex')
+    var genero = ''
+
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    /* console.log(input_radio) */
+
+    if(input_radio[0].checked) {
+       genero = 'Homem'
+
+       if(idade >= 0 && idade < 12){
+        img.setAttribute('src', 'imagens/criança.jpg')
+      
+       } else if (idade < 21) {
+        img.setAttribute('src', 'imagens/Jovem.jpg')
+       } else if (idade < 50) {
+        img.setAttribute('src', 'imagens/Adulto.jpg')
+       } else {
+        img.setAttribute('src', 'imagens/Idoso.jpg')
+       }
+
+    } else {
+        genero = 'Mulher'
+
+    if(idade >= 0 && idade < 12){
+        img.setAttribute('src', 'imagens/criançaMulher.jpg')
+    } else if (idade < 21) {
+        img.setAttribute('src', 'imagens/JovemMulher.jpg')
+    } else if (idade < 50) {
+        img.setAttribute('src', 'imagens/AdultaMulher.jpg')
+    } else {
+        img.setAttribute('src', 'imagens/Idosamulher.jpg')
+    }
+
+
+    }
+
+    resultado.appendChild(img)
+}
